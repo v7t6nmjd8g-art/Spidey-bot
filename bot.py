66a83470a10235bfd2b2eb5cdc328fd8e5c5a91e
v7t6nmjd8g-art <v7t6nmjd8g@privaterelay.appleid.com>
2026-07-22 22:12:00 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+import feedparser
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True

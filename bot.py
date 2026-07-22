2f8e41a2edd26be83a89d40711bc5b5fee40baf3
v7t6nmjd8g-art <v7t6nmjd8g@privaterelay.appleid.com>
2026-07-22 21:25:01 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@ import discord
 from discord.ext import commands
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
-intents.message_content = true
+intents.message_content = True
 bot = commands.Bot(command_prefix="!" , intents=intents)
 @bot.event 
 async def on_ready ():
-  print(f"🕷️ Spidey bot est connecté en tant que {bot.user}"
+  print(f"🕷️ Spidey bot est connecté en tant que {bot.user}")
 @bot.command()
 async def ping(ctx):
   await ctx.send ("🏓 pong ! spidey bot fonctionne.")
+bot.run(TOKEN) 

353f3bd35d5aaff45afa9fa97eb070fd939a7348
v7t6nmjd8g-art <v7t6nmjd8g@privaterelay.appleid.com>
2026-07-22 22:16:02 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import feedparser
 TOKEN = os.getenv("DISCORD_TOKEN")
 RSS_URL = "https://rss.app/feeds/WtYR8fnVhYK9yQRI.xml"
@@ -10,6 +10,23 @@ bot = commands.Bot(command_prefix="!" , intents=intents)
 @bot.event 
 async def on_ready ():
   print(f"🕷️ Spidey bot est connecté en tant que {bot.user}")
+  @tasks.loop(minutes=5)
+async def check_leaks():
+    feed = feedparser.parse(RSS_URL)
+
+    if feed.entries:
+        channel = discord.utils.get(bot.get_all_channels(), name="codm-leaks")
+
+        if channel:
+            latest = feed.entries[0]
+            await channel.send(
+                f"🚨 Nouveau leak CODM !\n{latest.title}\n{latest.link}"
+            )
+
+@bot.event
+async def on_ready():
+    print(f"🕷️ Spidey bot est connecté en tant que {bot.user}")
+    check_leaks.start()
 @bot.command()
 async def ping(ctx):
   await ctx.send ("🏓 pong ! spidey bot fonctionne.")
